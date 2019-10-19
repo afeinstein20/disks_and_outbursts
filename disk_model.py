8dcff31600309797da19b5aa968a8d131152d870
Adina Feinstein <adina.d.feinstein@gmail.com>
2019-10-19 12:22:40 -0500

    new and shiny

--- disk_model.py
+++ disk_model.py
@@ -1,135 +1,187 @@
 import numpy as np
-import scipy as sc
-from tqdm import tqdm
 from datetime import date
+from scipy.special import erf
 from astropy import units as u
-import scipy.optimize as optimize
-from astropy import constants as const
-from scipy.interpolate import interp1d
+from astropy import constants as c
 from astropy.table import Table, Column
 
 __all__ = ['disk_model']
 
 class disk_model(object):
-    
-    def __init__(self, time=np.linspace(0, 1e6, 1000),
-                 r=50.0, z=4.5, mid_T=21, 
-                 M_star=1, kappa=1, sigma=40,
-                 zr=None):
-        """
-        Creates a .out file to run through astrochem.
 
+    def __init__(self, r=1, z=0.03,
+                 Mstar=1, gas_dist=2000,
+                 opacity=0.01,
+                 time=np.linspace(0,5,1000)):
+        """
         Parameters
+        ---------- 
+        r : radial distance [AU]
+        z : vertical distance from midplane [AU]
+        Mstar : mass of the host star [M_sun]
+        gas_dist : distribution of gas for surface density [g cm^-2]
+        opacity : opacity of the disk [cm^2 g]
+        time : array of times [days]
+
+        Atributes
         ----------
-        time : time array [years]
         r : radial distance [AU]
         z : vertical distance [AU]
-        mid_T : midplane temperature [K]
-        M_star : mass of star [M_sun]
-        kappa : opacity [cm^2 g]
-        sigma : surface density [g cm^-2]
-        zr : z/r, optional
+        Mstar : mass of the host star [M_sun]
+        Lstar : luminosity of the host star [L_sun]
+        time : array of times [years, unless units otherwise specified]
         """
+        self.r = r*u.AU
+        self.z = z*u.AU
+        self.opacity = opacity * u.cm**2 * u.g
+
+        self.midplane_temp()
+        self.Tatm = 3*self.Tmid
+
+        self.Mstar = Mstar*c.M_sun
+        self.Lstar = 0.23 * (self.Mstar/c.M_sun).value**2.3 * c.L_sun
+
         self.time = time
         if type(time) != u.quantity.Quantity:
-            self.time *= u.year
-
-        self.t_gas_mid = mid_T * u.K
-        self.t_gas_atm = 3.0 * mid_T * u.K
-
-        self.sigma = sigma * u.g / u.cm**2
-        self.kappa = kappa * u.g * u.cm**2
-
-        self.M_star = (M_star * const.M_sun).to(u.g)
-
-        self.L_star = 0.23 * M_star**2.3 * const.L_sun
-        self.L_star_time = np.full(self.time.shape, self.L_star.value)
-        self.L_star_time_flare = None
-
-        self.fn = None
-
-        self.r = (r * u.AU).to(u.cm)
-
-        if zr is not None:
-            self.z = zr * self.r
-        else:
-            self.z = (z * u.AU).to(u.cm)
+            self.time *= u.day
 
         self.scale_height()
-        self.density_init()
-        self.density_rad()
+        self.init_density(gas_dist * u.g / u.cm**2)
+        self.density()
+        self.optical_depth()
+        self.temperature()
 
-        tvals = self.z / (self.H * np.sqrt(2))
-        self.optical_depth(sc.special.erfc(tvals))
+        self.lum_flare = None
+        self.flare = None
+        self.lum_UV = None
 
-        self.cosmic_rays()
+        return
+
+
+    def midplane_temp(self):
+        """
+        Defines the initial midplane temperature.
+
+        Attributes
+        ---------- 
+        Tmid : [K]
+        """
+        Tmid = 150*u.K / np.sqrt(self.r.to(u.AU).value)
+        self.Tmid = Tmid
 
 
     def scale_height(self):
         """
+        Calculates the scale height at disk radius r.
+
         Attributes
-        ----------
+        ---------- 
         H : scale height [cm]
         """
-        num = np.sqrt( const.k_B * self.t_gas_mid / (2.3*const.m_p))
-        num = num.to(u.m/u.s)
-        denom = np.sqrt(const.G * self.M_star / self.r**3)
-        H = num/denom
-        self.H = H.to(u.cm)
+        m_hyd   = c.m_p
+        k_boltz = c.k_B
 
-    def density_init(self):
-        """
-        Attributes
-        ----------
-        rho_o : initial density [g cm^-3]
-        """
-        rho_o = self.sigma/ (np.sqrt(2 * np.pi) * self.H)
-        self.rho_o = rho_o.to(u.g/u.cm**3)
-    
-    def density_rad(self):
-        """
-        Attributes
-        ----------
-        rho : density [g cm^-3]
-        """
-        exp = (-self.z**2)/(2.0 * self.H**2)
-        rho = self.rho_o * np.exp(exp.value)
-        self.rho = rho
-    
-    def optical_depth(self, taus):
-        """
-        Attributes
-        ----------
-        tau : optical depth [g^2]
-        """
-        od = np.sqrt(np.pi * self.H**2 * 0.5) * self.kappa * self.rho_o * taus
-        self.tau = od
-    
-    def cosmic_rays(self):
-        """
-        Attributes
-        ----------
-        cr : cosmic ray??
-        """
-        cosmic_constant = 5.0e-17
-        exp_denom = 96.0 * u.g / u.cm**2
-        self.cr = cosmic_constant * np.exp(-self.sigma/exp_denom)
+        num   = k_boltz * self.Tmid * self.r**3
+        denom = 2.3 * m_hyd * c.G * self.Mstar
+
+        self.H = np.sqrt(num/denom).to(u.cm)
         
 
-    def inject_flare(self, amp=100, t0_ind=200,
-                     rise=0.000003, fall=0.02, 
-                     xray=False, base_xray=10):
+    def init_density(self, gas_dist):
         """
-        Injects a Gaussian decay in the luminosity of the star.
+        Calculates the initial density in the disk.
+
+        Attributes
+        ---------- 
+        rho_o : [g cm^-3]
+        """
+        sigma_o = self.surface_density(r=0*u.AU, gas_dist=gas_dist)
+        rho_o = sigma_o / (np.sqrt(2*np.pi) * self.H)
+        self.rho_o = rho_o.to(u.g / u.cm**3)
+
+
+    def density(self):
+        """
+        Calculates the density at a given distance from the midplane.
+
+        Attributes
+        ---------- 
+        rho : [g cm^-3]
+        """
+        fraction = ((self.z/self.H).to(u.AU/u.AU))**2
+        rho = self.rho_o * np.exp( -0.5 * fraction )
+        self.rho = rho.to(u.g / u.cm**3)
+        
+
+    def surface_density(self, r, gas_dist):
+        """
+        Calculates the surface gas density at a given
+        radial distance, r.
+
+        Parameters
+        ----------
+        gas_dist : float, optional
+             The distribution of surface gas density. 
+             Default is 2000 [g cm^-2].
+
+        Returns
+        ---------- 
+        surface density : [g cm^-2]
+        """
+        r = r.to(u.AU)
+
+        if r.value == 0:
+            return gas_dist
+        else:
+            return gas_dist / r.value
+
+
+    def optical_depth(self):
+        """
+        Calculates the optical depth.
 
         Parameters
         ---------- 
-        amp : float, amplitude of the increase in brightness.
-        t0_ind : int, where the flare occurs in the time array.
-        rise : float, the steepness of the rise of the flare.
-        fall : float, the steepness of the decay of the flare.
-        xray : bool, returns a flare to feed into the chi value.
-        base_xray : float, base x-ray flux value.
+        opacity : opacity of the disk [cm^2 g]
+
+        Attributes
+        ---------- 
+        tau : [g^2]
+        """
+        term1   = np.sqrt( (np.pi * self.H**2) / 2)
+        errfunc = erf(self.z / (np.sqrt(2) * self.H.to(u.AU)) )
+        tau     = term1 * self.opacity * self.rho_o * errfunc
+        self.tau = tau.to(u.g**2)
+
+
+    def temperature(self, zq=3.0, delta=2.0):
+        """
+        Describes the temperature at some radial distance, r, and
+        some vertical distance, z, by a piece-wise function.
+
+        Parameters
+        ---------- 
+        zq : modifying parameter for the atmosphere of the disk.
+             Default = 3.
+        delta : factor in defining the temperature profile. 
+             Default = 2.
+
+        Attributes
+        ---------- 
+        T : [K]
+        """
+        if self.z < zq*self.H:
+            fraction = (self.z/self.H).to(u.AU/u.AU)
+            sin = np.sin( np.pi / (2*zq) * fraction.value )
+            sin = (self.Tatm - self.Tmid) * sin**(2*delta)
+            self.T = self.Tmid + sin
+        else:
+            self.T = self.Tatm
+        
+
+    def flare_model(self, amp, t0_ind, rise, fall):
+        """
+        Creates the flare model.
         """
         def gauss_rise(time, amp, t0, rise):
             return amp * np.exp( -(time - t0)**2.0 / (2.0*rise**2.0) )
@@ -139,110 +191,128 @@ class disk_model(object):
 
         time = self.time.value
 
-        if xray:
-            baseline = base_xray
-        else:
-            baseline = self.L_star.value
-
         growth = np.where(time <= time[t0_ind])[0]
         decay  = np.where(time >  time[t0_ind])[0]
-
-        rise = gauss_rise(time[growth], amp*baseline,
+            
+        rise = gauss_rise(time[growth], amp,
                           time[t0_ind], rise)
-        fall = exp_decay( time[decay] , amp*baseline,
+        fall = exp_decay( time[decay] , amp,
                           time[t0_ind], fall)
 
-        if xray is False:
-            self.L_star_time_flare = self.L_star_time + np.append(rise, fall)
-            self.L_star_time_flare *= u.W
-        else:
-            self.xray_change = np.append(rise, fall)+baseline
-            self.xray_factor = baseline*amp
+        flare = np.append(rise, fall)
 
-    def calc_temp(self, z_q=2, d=3, flare=True):
+        return flare+1
+
+
+    def luminosity_flare(self, amp=100, t0_ind=200,
+                         rise=0.0002, fall=0.03):
         """
-        Calculates the temperature in the disk.
+        Injects a flare-like shape into the luminosity of the star
+        over time. Can pass in a list of all parameters for multiple
+        flares to be injected.
 
         Parameters
         ---------- 
-        z_q : 
-        d : 
-        flare : bool, tells which luminosity to use.
+        amp : the amplitude of the flare compared to the base luminosity.
+              Default is 100 times brighter during the flare.
+        t0_ind : where the peak of the flare will occur. Default is 
+                 at the beginning of the time array.
+        rise : defines the rise timescale of the flare. Default = 0.0002.
+        fall : defintes the decay timescale of the flare. Default = 0.02.
+        multi : bool, allows the user to inject multiple flares.
+                Will overwrite attributes set in this function.
+
+        Attributes
+        ----------
+        flare : the flare model injected into the luminosity.
+        lum_flare : array of luminosity with flare(s) injection.
+        delta_T : changing temperature with the luminosity
+        """
+        if type(amp) == int or type(amp) == float:
+            flare = self.flare_model(amp, t0_ind, rise, fall)
+            self.flare = flare
+            self.lum_flare = self.Lstar * self.flare
+            
+        else:
+            self.flare = np.ones(self.time.shape)
+            self.lum_flare = np.full(self.time.shape, self.Lstar)
+            for i in range(len(amp)):
+                flare = self.flare_model(amp[i]-1, t0_ind[i], rise[i], fall[i])
+                self.flare *= flare
+
+            self.lum_flare = self.Lstar * self.flare
+
+        self.delta_T = self.T * ((self.lum_flare / c.L_sun)**0.25).value
+
+
+    def UV_flare(self, base=10, factor=100):
+        """
+        Defines an array of chi (UV Flux) values for 
+        the input.ini file.
+        
+        Parameters
+        ---------- 
+        base : the base chi value for the UV energy.
+        factor : the conversion between white light flare
+             energy and energy in the UV. Default = 100.
 
         Attributes
         ---------- 
-        temp : temperature [K]
+        lum_UV : array of chi values [unitless]
         """
-        temp = np.zeros(len(self.time))
-
-        if flare is True:
-            if self.L_star_time_flare is None:
-                raise ValueError("Please call disk_model.inject_flare() first.")
-            else:
-                L_star = self.L_star_time_flare
+        if self.flare is None:
+            raise ValueError("Please call disk_model.luminosity_flare() to set flares.")
         else:
-            L_star = self.L_star_time
+            self.lum_UV = ((self.flare-1) * factor) + base
 
-        for i in tqdm(range(len(self.time))):
-            del_lum = (L_star[i]/const.L_sun)**0.25
-
-            if self.z < z_q*self.H:
-                t1 = self.t_gas_atm - self.t_gas_mid
-                t2 = np.sin( ( (np.pi*self.z.to(u.AU))/(2.0*z_q*self.H.to(u.AU)) ).value)**(2.0*d)
-                t = self.t_gas_mid + t1*t2
-            else:
-                t = self.t_gas_atm
-                
-            temp[i] = t.to(u.K).value * del_lum
-
-        self.temp = temp
-
-
-    def default_file(self, xray):
-        """
-        Creates a default file name if one is not passed in.
+        
+    def default_file(self):
+        """                                                                                                     
+        Creates a default file name if one is not passed in.                                                    
         """
         today = date.today().strftime("%d%m%Y")
         fn = '{0}_Mstar{3}_r{1}_z{2}'.format(today,
                                              self.r.to(u.AU).value,
                                              self.z.to(u.AU).value,
-                                             np.round(self.M_star.to(const.M_sun).value,2))
+                                             np.round(self.Mstar.to(c.M_sun).value,2))
         self.fn = fn
-        return
-        
 
-    def write_path_file(self, fn=None, alpha=0.001):
+
+    def write_path_file(self, fn=None):
         """
         Creates the .out file with parameters.
-        Columns are: time, r, z, n, tgas, alpha, opt_depth.
+        Columns are: time, luminosity, UV luminosity, r, z, n, tgas, alpha, tau
         """
         if fn is None:
-            self.default_file(False)
-            fn = self.fn + '.out'
+            self.default_file()
         else:
             self.fn = fn.split('.')[0]
 
+        if self.lum_flare is None:
+            fn = self.fn + '_noflare.out'
+            luminosity = np.full(self.time.shape, self.Lstar)
+            UV_lum = np.full(self.time.shape, 100)
+            temp_array = np.full(self.time.shape, self.T)
+        else:
+            fn = self.fn + '_flare.out'
+            luminosity = self.lum_flare
+
+            if self.lum_UV is None:
+                raise ValueError("You forgot to update the UV flux with your flare.")
+            
+            UV_lum = self.lum_UV
+            temp_array = self.delta_T
+                
         t = Table()
         t.add_column(Column(data=self.time, name='time'))
+        t.add_column(Column(data=luminosity, name='lum'))
+        t.add_column(Column(data=UV_lum, name='UV_lum'))
         t.add_column(Column(data=np.full(self.time.shape, self.r.to(u.AU).value), name='r'))
         t.add_column(Column(data=np.full(self.time.shape, self.z.to(u.AU).value), name='z'))
-        t.add_column(Column(data=np.full(self.time.shape, self.rho.to(u.g/u.cm**3).value), name='n'))
-        t.add_column(Column(data=self.temp, name='tgas'))
-        t.add_column(Column(data=np.full(self.time.shape, alpha), name='alpha'))
+        t.add_column(Column(data=np.full(self.time.shape, self.rho.to(u.g/u.cm**3).value), 
+                            name='n'))
+        t.add_column(Column(data=temp_array, name='tgas'))
+        t.add_column(Column(data=np.full(self.time.shape, self.opacity.value), name='alpha'))
         t.add_column(Column(data=np.full(self.time.shape, self.tau.value), name='opt_depth'))
 
         t.write(fn, format='ascii')
-
-        return
-
-
-    def write_chi_file(self):
-        """
-        Creates a file with the change in UV flux.
-        """
-        if self.fn is None:
-            raise ValueError("Please create .out file to go with this x-ray change.")
-        
-        t = Table()
-        t.add_column(Column(data=self.xray_change, name='chi'))
-        t.write(self.fn+'_xray{0}.txt'.format(self.xray_factor), format='ascii')
